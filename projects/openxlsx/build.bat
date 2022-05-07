d685b6ff9ba25b614e5a4dc1a3d5057c36d0f7d2
uniqs <uniqs@163.com>
2022-05-07 19:08:42 +0800

    shell optimize

--- projects/openxlsx/build.bat
+++ projects/openxlsx/build.bat
@@ -12,20 +12,9 @@ cmake -A x64 -DCMAKE_BUILD_TYPE=%BAT_BUILD_TYPE% ..
 
 cmake --build . --config %BAT_BUILD_TYPE%
 
-@REM cmake --install . --prefix ..\..\..\ --config %BAT_BUILD_TYPE%
+cmake --install . --prefix ..\..\..\ --config %BAT_BUILD_TYPE%
 
 cd ..
-
-mkdir ..\..\include\openxlsx\
-cp -R .\OpenXLSX\OpenXLSX.hpp ..\..\include\openxlsx\
-
-mkdir ..\..\include\openxlsx\headers\
-cp -R ./OpenXLSX/headers/*.hpp ..\..\include\openxlsx\headers\
-
-cp -R .\build\OpenXLSX\OpenXLSX-Exports.hpp ..\..\include\openxlsx\
-
-mkdir ..\..\lib\
-cp -R ./build/output/%BAT_BUILD_TYPE%/*.lib ..\..\lib\
 
 mkdir ..\..\..\bin\%BAT_BUILD_TYPE%\
 cp -R ./build/output/%BAT_BUILD_TYPE%/*.dll ..\..\..\bin\%BAT_BUILD_TYPE%\

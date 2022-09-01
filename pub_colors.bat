f366f3328d2e7eb7b06b6fdbd2be117077f36489
uniqs <uniqs@163.com>
2022-09-01 20:31:19 +0800

    pub

--- pub_colors.bat
+++
@@ -1,3 +0,0 @@
-xcopy .\*.exe D:\work\modesttech\colors\colors_design\uniqs_cfg_generator\ /S /Q /Y
-xcopy .\*.dll D:\work\modesttech\colors\colors_design\uniqs_cfg_generator\ /S /Q /Y
-xcopy .\*.bat D:\work\modesttech\colors\colors_design\uniqs_cfg_generator\ /S /Q /Y
